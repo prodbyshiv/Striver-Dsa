b03069ac945460b15df905447e7c4d846463e540
shivam <shivamtiwarisgbt@gmail.com>
2025-09-01 20:16:04 +0530

    first commit

---
+++ Recursion/printnamentimes.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void printName(int i, int n){
+    if (i > n)
+    {
+        return;
+    }
+    cout<< "shivam"<<endl;
+    printName(i+1,n);
+}
+
+int main(){
+    printName(1,7);
+}
