71faa21ccb3addf3ebc0c8b89c610e71d1ec8c64
shivam <shivamtiwarisgbt@gmail.com>
2025-09-20 21:41:21 +0530

    max-subarray-brute force solution

---
+++ Array/subarray.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+// BRUTE FORCE SOLUTION
+int longestSubarray(vector<int> &nums, int k){
+    int maxlength = 0;
+    for(int i = 0; i < nums.size(); i++){
+        int sum = 0; 
+        for(int j = i; j < nums.size(); j++){
+            sum = sum + nums[j];
+            if(sum == k){
+                maxlength = max(maxlength, j - i + 1);
+            }
+        }
+    }
+    return maxlength;
+}
+
+int main() {
+    int n, k;
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    vector<int> nums(n);
+    cout << "Enter array elements: ";
+    for(int i = 0; i < n; i++){
+        cin >> nums[i];
+    }
+
+    cout << "Enter value of k: ";
+    cin >> k;
+
+    int ans = longestSubarray(nums, k);
+    cout << "Longest Subarray Length with sum " << k << " is: " << ans << endl;
+
+    return 0;
+}
