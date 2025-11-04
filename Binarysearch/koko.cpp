265163b9053118478eb3db3a10926e2fb0d01043
shivam <shivamtiwarisgbt@gmail.com>
2025-11-04 09:06:56 +0530

    koko fix

--- Binarysearch/koko.cpp
+++ Binarysearch/koko.cpp
@@ -1,39 +1,48 @@
+// koko eating bananas
+
 #include <iostream>
 #include <vector>
 using namespace std;
 
+int findMax(vector<int> &piles){
+    int maxi = 0;
+    for (int i = 0; i < piles.size(); i++)
+    {
+        maxi = max(maxi,piles[i]);
+    }
+    return maxi;
+}
+
+ int inttotalHours(vector<int> &piles,int h){
+    int totalH = 0;
+    for (int i = 0; i < piles.size(); i++)
+    {
+        totalH += ceil((double)piles[i]/(double)h);
+    }
+    return totalH;
+    
+}
+
+int minimumSpeed(vector<int> &piles, int h){
+    
+int low = 1; int high = findMax(piles);
+int mid = (low+high)/2;
+int totalH = inttotalHours(piles,mid);
+
+if (totalH <= h)
+{
+    high = mid-1;
+}
+else{
+    low = mid+1;
+}
+return low;
+}
+
 int main() {
-   vector<int>piles = {3,6,7,11};
-   int h = 8;
-   int low = 1;
-   int high = 11;
-   int ans = high;  
+vector<int> piles = {3,6,7,11};
+ int h=8;
 
-   while (low <= high)
-   {
-        int totalHours = 0; // current kitna der me kha rhi hai?
-        int mid = (low+high)/2; 
-
-
-
-        for (int i = 0; i < piles.size(); i++)
-        {
-            totalHours += ceil((double)piles[i] / mid); 
-        }
-        
-        if (totalHours <= h)
-        {
-            ans = mid;
-            high = mid-1;
-        }
-        else
-        {
-            low = mid+1;
-        }
-        
-        
-   }
-   
-
-    cout << "Minimum speed needed: " << ans << endl;
+    
+    
 }
